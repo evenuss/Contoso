f5cf6f605a45e85c34e65dd889635ad7531dfe1a
evenuss <ucoksilalahi345@gmail.com>
2019-12-30 13:55:38 +0700

    push heroku

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ mail_settings = {
 
 
 app.secret_key = "SECRET_KEY"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/contoso"
+app.config["MONGO_URI"] = "mongodb+srv://evenuss:arjuna203@cluster0-sxt0m.gcp.mongodb.net/contoso"
 app.config['JWT_SECRET_KEY'] = 'SECRET_KEY'
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 app.config.update(mail_settings)
