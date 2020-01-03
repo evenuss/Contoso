3cd0e659f9414b96b58c0d3b24ddba89cfb20703
evenuss <ucoksilalahi345@gmail.com>
2020-01-03 14:35:47 +0700

    Revisi File

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ mail_settings = {
 
 
 app.secret_key = "SECRET_KEY"
-app.config["MONGO_URI"] = "mongodb+srv://evenuss:arjuna203@cluster0-sxt0m.gcp.mongodb.net/contoso"
+# app.config["MONGO_URI"] = "mongodb+srv://evenuss:arjuna203@cluster0-sxt0m.gcp.mongodb.net/contoso"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/contoso"
 app.config['JWT_SECRET_KEY'] = 'SECRET_KEY'
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 app.config.update(mail_settings)
